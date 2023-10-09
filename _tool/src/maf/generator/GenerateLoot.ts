e6c8d7af15d2e4de8671dda5971cf2642afebc3e
para7 <cometpara7@gmail.com>
2023-10-09 22:57:52 +0900

    フルルートテーブル

--- _tool/src/maf/generator/GenerateLoot.ts
+++ _tool/src/maf/generator/GenerateLoot.ts
@@ -3,11 +3,13 @@ import { DBSchemaType } from "../zod/db";
 // 全部落とすルートテーブルを作成
 
 const GenerateLine = (x: DBSchemaType["data"][number]) => {
-  return `{"rolls": 1,"entries": [{"type": "minecraft:item","name": "minecraft:book","functions": [{"function": "minecraft:set_nbt","tag": "{Enchantments:[{}],grimoire:1,magicID:${x.castid}}"},{"function": "minecraft:set_name","name": "${x.title}"}]}]}`;
+  return `{"type": "minecraft:item","name": "minecraft:book","functions": [{"function": "minecraft:set_nbt","tag": "{Enchantments:[{}],grimoire:1,magicID:${x.castid}}"},{"function": "minecraft:set_name","name": "${x.title}"}]}`;
+  // return `{"rolls": 1,"entries": []}`;
 };
 
 export const GenerateFullLoot = (_data: DBSchemaType) => {
-  const templateGen = (x: string) => `{"type": "minecraft:entity","pools": [${x}]}`;
+  const templateGen = (x: string) =>
+    `{"type": "minecraft:entity","pools": [{"rolls": 1,"entries": [${x}]}]}`;
 
   const value = _data.data.map(GenerateLine).join(",");
 
