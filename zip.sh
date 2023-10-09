78a30e4a3a143ec4f2149c645f80c651a55e1bcb
para7 <cometpara7@gmail.com>
2023-10-10 00:42:30 +0900

    zip生成場所変更

--- zip.sh
+++ zip.sh
@@ -1,19 +1,19 @@
 cd devtool
 rm devtool.zip
-zip -r ../devtool.zip *
+zip -r ../zip/devtool.zip *
 cd ../
 
 cd MagicAndFrontier
 rm MagicAndFrontier.zip
-zip -r ../MagicAndFrontier.zip *
+zip -r ../zip/MagicAndFrontier.zip *
 cd ../
 
 cd MultiPlayTools
 rm MultiPlayTools.zip
-zip -r ../MultiPlayTools.zip *
+zip -r ../zip/MultiPlayTools.zip *
 cd ../
 
 cd p7BaseSystem
 rm p7BaseSystem.zip
-zip -r ../p7BaseSystem.zip *
+zip -r ../zip/p7BaseSystem.zip *
 cd ../
