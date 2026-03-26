e7675d69986fb86f6e95ea5d6be34296877e572b
para7 <18499975+para7@users.noreply.github.com>
2026-03-26 13:48:04 +0900

    fix warn

--- maf-command-generator/app/cli/validate.go
+++ maf-command-generator/app/cli/validate.go
@@ -18,19 +18,4 @@ func Validate(cfg files.MafConfig) int {
 		return 1
 	}
 	return 0
-
-	// entity := grimoire.NewGrimoireEntity(cfg.GrimoireStatePath)
-	// if err := entity.Load(); err != nil {
-	// 	fmt.Fprintf(os.Stderr, "failed to load grimoire: %v\n", err)
-	// 	return 1
-	// }
-	// fmt.Printf("Loaded %d grimoires\n", len(entity.GetAll()))
-	// errs := entity.ValidateAll(nil)
-	// for _, e := range errs {
-	// 	fmt.Fprintln(os.Stderr, e)
-	// }
-	// if len(errs) > 0 {
-	// 	return 1
-	// }
-	return 0
 }
